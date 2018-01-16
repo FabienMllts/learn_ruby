df221b862d4ba38ad73c1d938d7f8a816c617b45
FabienMllts <fabienmallet@gmail.com>
2018-01-16 15:15:37 +0100

    Exo simon says with comments

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -42,20 +42,20 @@ def titleize(word)
 end
 
 #doesn't capitalize 'little words' in a title
- #does capitalize 'little words' at the start of a title
+#does capitalize 'little words' at the start of a title
 
 
 def titleize(welcome)
-	
+	# Stock the little words expected
 	little_words = ["the", "and", "over"]
 	final_word = welcome.split(" ")
-
+#creation de loops with the ! variable modified
 	final_word.each do |word|
 		word.capitalize! if !little_words.include?(word)
 	end
-
+#fonction final_word with a method capitalize
 	final_word[0].capitalize!
-
+#return final_word wiht method join
 	return final_word.join(" ")
 
 end
