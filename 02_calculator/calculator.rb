30788e64606c221589003121cc4b7c928be5474d
FabienMllts <fabienmallet@gmail.com>
2018-01-16 13:34:03 +0100

    Exo calculator sans erreurs et sans les bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+
+
+def add(number1, number2)
+	number1 + number2
+end
+
+
+def subtract(number1, number2)
+	number1 - number2
+end
+
+
+def sum(numbers)
+	total = 0
+	numbers.each do |x| total += x
+	 end	
+	 return total
+end
+
+=begin
+def multiply(numbers)
+	total = 1
+	numbers.each { |number| total *= number }
+	return total
+end
+
+def power(number1, number2)
+	number1 ** number2
+end
+
+def factorial(number_to_facto)
+	return 0 if number_to_facto == 0
+	total = 1
+	(1..number_to_facto).select {|number| total *= number}
+	return total
+end
+=end
