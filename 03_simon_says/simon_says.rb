662b8741c32e4d7fe176ffbcb2f5d87b6cc4a312
FabienMllts <fabienmallet@gmail.com>
2018-01-16 15:00:35 +0100

    Exo simon_says sans erreurs

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,61 @@
 #write your code here
+#Echo
+
+def echo(hello)
+	hello
+end
+
+#Shout
+
+def shout(hello)
+	hello.upcase
+end
+
+#Multiples words
+
+def shout_multiple_words(str)
+	words = str.upcase.split(' ')
+end
+
+#Repeat
+
+def repeat(hello, times = 2)
+	 ((hello + " ") * times).rstrip
+end
+
+#Start of word
+
+def start_of_word(hello, number)
+	hello[0..number-1]
+end
+
+#First word
+
+def first_word(hello)
+	hello.split(" ")[0]
+end
+
+#Capitalize a word
+
+def titleize(word)
+	word.capitalize
+end
+
+#doesn't capitalize 'little words' in a title
+ #does capitalize 'little words' at the start of a title
+
+
+def titleize(welcome)
+	
+	little_words = ["the", "and", "over"]
+	final_word = welcome.split(" ")
+
+	final_word.each do |word|
+		word.capitalize! if !little_words.include?(word)
+	end
+
+	final_word[0].capitalize!
+
+	return final_word.join(" ")
+
+end
