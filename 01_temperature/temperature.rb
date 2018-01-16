0f0368541234e3f17bea5d21f352823fdd48336f
FabienMllts <fabienmallet@gmail.com>
2018-01-16 12:15:36 +0100

    Exo Température sans erreurs

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,15 @@
 #write your code here
+
+# convert fahrenheit to celcius
+def ftoc(f_degree)
+
+	(f_degree - 32.0) * 5.0 / 9.0
+
+end
+
+# convert celcius to fahrenheit
+def ctof(c_degree)
+
+	(c_degree * 9.0 / 5.0) + 32.0
+
+end
